10b3e7aa8a4dafd97671f60f4333c36c97893e2a
Prasad Sodagudi <psodagud@codeaurora.org>
2016-02-26 15:41:24 +0530

    linux-ramdump-parser-v2: Add support for APQ8953
    Add support for detecting APQ8953 target
    and parsing the ramdumps.
    
    Change-Id: I9529d2187ce70401d57ca14c4294c4a7cee2c810

--- linux-ramdump-parser-v2/boards.py
+++ linux-ramdump-parser-v2/boards.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
+# Copyright (c) 2014-2016, The Linux Foundation. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 and
@@ -531,6 +531,7 @@ boards.append(Board8937(socid=294))
 boards.append(Board8937(socid=295))
 
 boards.append(Board8953(socid=293))
+boards.append(Board8953(socid=304))
 
 def get_supported_boards():
     """ Called by other part of the code to get a list of boards """
