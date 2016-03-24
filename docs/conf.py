d01bed4aeadf89c8aa31c3a8b6f9cbf8572b20a0
Mitchel Humpherys <mitchelh@codeaurora.org>
2016-03-23 18:08:22 -0700

    lrdp-v2: docs: Set autoclass_content = 'both'
    This is needed to allow __init__ methods to be documented along with
    their class definition.
    
    Change-Id: I7371bd038e3898cc97282c75e20981d9fd1cb9a5

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2015, The Linux Foundation. All rights reserved.
+# Copyright (c) 2015-2016, The Linux Foundation. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 and
@@ -259,3 +259,5 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 # texinfo_no_detailmenu = False
+
+autoclass_content = 'both'
